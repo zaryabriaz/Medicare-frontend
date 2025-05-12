452141408206108346327e5d10514eb797058106
zaryabriaz <zaryabriaz@gmail.com>
2025-05-12 17:35:47 +0500

    Added new pages for main site

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,49 @@
-import React from 'react'
-
 export const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
+  return <section>
+    <div className='px-4 mx-auto max-w-screen-md'>
+      <h2 className='heading text-center'>Contact Us</h2>
+      <p className='mb-8 lg:mb-16 font-light text-center text__para'> 
+        Got a technical issue? Want to send feedback about a beta feature? Let us know.
+      </p>
+      <form action="#" className='space-y-8'>
+        <div>
+          <label htmlFor="email" className='form__label'>
+            Your Email
+          </label>
+          <input
+           type="email"
+           id="email"
+           placeholder='example@gmail.com'
+           className='form__input mt-1'
+           />
+        </div>
+
+        <div>
+          <label htmlFor="subject" className='form__label'>
+            Subject
+          </label>
+          <input
+           type="text"
+           id="subject"
+           placeholder='Let us know we can help you'
+           className='form__input mt-1'
+           />
+        </div>
+
+        <div className="sm:col-span-2">
+          <label htmlFor="subject" className='form__label'>
+            Subject
+          </label>
+          <textarea
+           type="text"
+           rows="6"
+           id="message"
+           placeholder='Leave a comment....'
+           className='form__input mt-1'
+           />
+        </div>
+        <button type="submit" className="btn rounded sm:w-fit">Submit</button>
+      </form>
+    </div>
+  </section>
 }
