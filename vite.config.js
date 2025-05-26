d1a22b24419e4603d6923179f2ea672d0cf6fedc
zaryabriaz <zaryabriaz@gmail.com>
2025-05-26 17:34:04 +0500

    Updated the base url

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = import.meta.env.VITE_API_URL || '/api/v1'
+export const BASE_URL = 'https://medicare-backend-production-0b0c.up.railway.app/api/v1'
 
 export const getToken = () => {
     const token = localStorage.getItem('token')
